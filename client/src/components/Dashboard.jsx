4b59c868957849261648af9261eeb9293c660b6d
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-28 11:48:25 -0700

    render dynamic developer tasks using state

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,44 +1,71 @@
+import { useState } from "react";
+
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import TaskCard from "./TaskCard";
 import TaskForm from "./TaskForm";
-import { useState } from "react";
 
 function Dashboard() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Build REST API",
-      completed:false,
-    },
-    {
-        id:2,
-        title:"Learn MongoDB",
-        completed:true,
-    },
-  ]);
 
-  return (
-    <div>
-      <Navbar />
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            title: "Build REST API",
+            completed: false,
+        },
 
-      <div className="flex">
-        <Sidebar />
+        {
+            id: 2,
+            title: "Learn MongoDB",
+            completed: true,
+        },
 
-        <div className="flex-1 p-6 bg-gray-100 min-h-screen">
-          <h1 className="text-3xl font-bold mb-6">Developer Tasks</h1>
+        {
+            id: 3,
+            title: "Deploy Backend Project",
+            completed: false,
+        },
+    ]);
 
-          <TaskForm />
+    return (
 
-          <div className="grid grid-cols-3 gap-5">
-            <TaskCard />
-            <TaskCard />
-            <TaskCard />
-          </div>
+        <div>
+
+            <Navbar />
+
+            <div className="flex">
+
+                <Sidebar />
+
+                <div className="flex-1 p-6 bg-gray-100 min-h-screen">
+
+                    <h1 className="text-3xl font-bold mb-6">
+                        Developer Tasks
+                    </h1>
+
+                    <TaskForm />
+
+                    <div className="grid grid-cols-3 gap-5">
+
+                        {
+                            tasks.map((task) => (
+
+                                <TaskCard
+                                    key={task.id}
+                                    task={task}
+                                />
+
+                            ))
+                        }
+
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Dashboard;
