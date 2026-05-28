00f018d44ff45d4682dc1c0477d90aebbc0641b0
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-28 11:43:41 -0700

    setup frontend task state management

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -2,41 +2,43 @@ import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import TaskCard from "./TaskCard";
 import TaskForm from "./TaskForm";
+import { useState } from "react";
 
 function Dashboard() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Build REST API",
+      completed:false,
+    },
+    {
+        id:2,
+        title:"Learn MongoDB",
+        completed:true,
+    },
+  ]);
 
-    return (
+  return (
+    <div>
+      <Navbar />
 
-        <div>
+      <div className="flex">
+        <Sidebar />
 
-            <Navbar />
+        <div className="flex-1 p-6 bg-gray-100 min-h-screen">
+          <h1 className="text-3xl font-bold mb-6">Developer Tasks</h1>
 
-            <div className="flex">
+          <TaskForm />
 
-                <Sidebar />
-
-                <div className="flex-1 p-6 bg-gray-100 min-h-screen">
-
-                    <h1 className="text-3xl font-bold mb-6">
-                        Developer Tasks
-                    </h1>
-
-                    <TaskForm />
-
-                    <div className="grid grid-cols-3 gap-5">
-
-                        <TaskCard />
-                        <TaskCard />
-                        <TaskCard />
-
-                    </div>
-
-                </div>
-
-            </div>
-
+          <div className="grid grid-cols-3 gap-5">
+            <TaskCard />
+            <TaskCard />
+            <TaskCard />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
