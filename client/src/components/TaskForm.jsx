8e40c6946cd7f979e6bc47d03a729a44c4d5f780
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-28 11:36:56 -0700

    create developer task form component

---
+++ client/src/components/TaskForm.jsx
@@ -0,0 +1,29 @@
+function TaskForm(){
+
+    return(
+
+        <div className="bg-white p-5 rounded-xl shadow-lg mb-6">
+
+            <h2 className="text-2xl font-bold mb-4">
+                Add New Task
+            </h2>
+
+            <form className="flex gap-4">
+
+                <input
+                type="text"
+                placeholder="Enter task"
+                className="border p-3 rounded w-full"
+                />
+
+                <button className="bg-blue-500 text-white px-5 rounded">
+                    Add
+                </button>
+
+            </form>
+
+        </div>
+    );
+}
+
+export default TaskForm;
