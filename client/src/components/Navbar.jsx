ba252716493f50151811ff0b9194f184bfdce769
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-29 12:25:20 -0700

    Added Hover effect in the Add Task button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,13 @@
-function Navbar(){
-    return(
-        <nav className="bg-black text-white p-4 flex justify-between items-center">
+function Navbar() {
+  return (
+    <nav className="bg-black text-white p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold">DevTrack AI</h1>
 
-            <h1 className="text-2xl font-bold">
-                DevTrack AI
-            </h1>
-
-            <button className="bg-blue-500 px-4 py-2 rounded">
-                Add Task
-            </button>
-
-        </nav>
-    )
+      <button className="bg-blue-500 px-4 py-2 rounded text-white hover:bg-blue-600 transition duration-300">
+        Add Task
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar;
