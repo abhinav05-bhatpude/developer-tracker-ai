ad051e4b03cb265fdea3e3f13e615618c0cbfe09
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-05-29 11:47:46 -0700

    create developer task search bar

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+function SearchBar(){
+
+    return(
+        <div className="mb-6">
+            <input
+            type="text"
+            placeholder="Search Developer tasks..."
+            className="w-full p-3 rounded-xl border"
+            />
+
+        </div>
+    );
+}
+
+export default SearchBar;
