09cddd34ec3658f477ac1f172fe79f47fc49e234
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-01 13:37:46 -0700

    create task creation API service

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,10 +1,23 @@
 import axios from "axios";
 
-const API_URL="http://localhost:5000/api/tasks";
+const API_URL =
+"http://localhost:5000/api/tasks";
 
-export const getTasks=async ()=>{
+export const getTasks = async () => {
 
-    const response=await axios.get(API_URL);
+    const response =
+    await axios.get(API_URL);
 
     return response.data;
 };
+
+export const createTask=
+async(task)=>{
+
+    const response=
+    await axios.post(
+        API_URL,
+        task
+    );
+    return response.data;
+}
