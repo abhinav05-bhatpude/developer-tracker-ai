3f4e9509352d96b3bb1c0b7ae0282e431300697d
abhinav05-bhatpude <abhinavbhatpude@gmail.com>
2026-06-01 13:19:46 -0700

    create task API service layer

---
+++ client/src/services/taskService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_URL="http://localhost:5000/api/tasks";
+
+export const getTasks=async ()=>{
+
+    const response=await axios.get(API_URL);
+
+    return response.data;
+};
